cdd936af2f2824693626ba06aab70e95e49f03d8
DrITCodEr <127026728+DrITCodEr@users.noreply.github.com>
2023-03-12 09:44:54 +0100

    Update IceCalc.py

--- IceCalc.py
+++ IceCalc.py
@@ -1,13 +1,16 @@
-# Dieses Programm berechnet die perfekten Zutanten für ein Eis
+# Dieses Programm berechnet die perfekten Zutaten für ein Eis
 # für die Eismaschine mit Dr. Almonds Eiscremezauber
 # DrITCodEr by 27th June
 
+# Begrüßungsnachricht
 print("**************************************************************************************\n")
 print("            * E * I * S * * * K * A * L * K * U * L * A * T * O * R * ! * \n")
 print("**************************************************************************************\n")
 
+# Auswahl der Eissorte
 Sorte = int(input("Welche Sorte Eis möchtest Du zubereiten?\n\nVanille  = 1\nSchoko   = 2\nErdbeere = 3\n\n"))
 
+# Verarbeitung der Eingabe
 if Sorte == 1:
     print("\nDu magst also Vanille, ich auch ;-)\n\n")
     MengeEis = int(input("\n Wieviel Vanille Eis (in g) möchtest Du zubereiten? "))
@@ -24,10 +27,9 @@ elif Sorte == 2:
     MengeKakao = MengeEis * 30 // 580
     print("\n Für %d g Schoko Eis benötigst Du: \n\n %d g Eiscremezauber \n %d g flüssige Sahne\n %d g Wasser und\n %d g Kakao Pulver.\n\n Guten Appetit!" % (MengeEis, MengeEiscremezauber, MengeSahne, MengeWasser, MengeKakao))
 elif Sorte == 3:
-    print("\nDu magst also Erbeere. Na klar, ich sowieso ;-) \n\n")
+    print("\nDu magst also Erdbeere. Na klar, ich sowieso ;-) \n\n")
     MengeEis = int(input("\n Wieviel Erdbeer Eis (in g) möchtest Du zubereiten? "))
     MengeEiscremezauber = MengeEis * 100 // 700
     MengeSahne = MengeEis * 300 // 700
     MengeErdbeer = MengeEis * 300 // 700
-    print("\n Für %d g Erdbeer Eis benötigst Du: \n\n %d g Eiscremezauber \n %d g flüssige Sahne und\n")
-    
+    print("\n Für %d g Erdbeer Eis benötigst Du: \n\n %d g Eiscremezauber \n %d g flüssige Sahne und\n %d g pürierte Erdbeeren.\n\n")
